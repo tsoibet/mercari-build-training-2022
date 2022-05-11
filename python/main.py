c4f6afde7acf2d64e15495d037350ac1475528b8
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-05-11 14:26:28 +0900

    Amendment: Complete task 4 in STEP3

--- python/main.py
+++ python/main.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-FILENAME = "items.json"
 DATABASE_NAME = "../db/mercari.sqlite3"
 
 app = FastAPI()
@@ -32,7 +31,7 @@ def database_connect():
     cur.execute(f'''{schema}''')
     conn.commit()
     logger.info("Database initialization complete.")
-    cur.close()
+    conn.close()
 
 @app.get("/")
 def root():
@@ -40,11 +39,21 @@ def root():
 
 @app.get("/items")
 def get_items():
-    return load_file_to_json()
+    conn = sqlite3.connect(DATABASE_NAME)
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM items''')
+    items = cur.fetchall()
+    conn.close()
+    logger.info("Get items")
+    return items
 
 @app.post("/items")
 def add_item(name: str = Form(...), category: str = Form(...)):
-    write_json({"name": name, "category": category})
+    conn = sqlite3.connect(DATABASE_NAME)
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO items(name, category) VALUES (?, ?)''', (name, category))
+    conn.commit()
+    conn.close()
     logger.info(f"Receive item: {name}")
     return {"message": f"item received: {name}"}
 
@@ -61,14 +70,3 @@ async def get_image(items_image):
         image = images / "default.jpg"
 
     return FileResponse(image)
-
-def load_file_to_json():
-    with open(FILENAME, 'r') as file:
-        file_data = json.load(file)
-    return file_data
-
-def write_json(new_data):
-    file_data = load_file_to_json()
-    with open(FILENAME, 'w') as file:
-        file_data["items"].append(new_data)
-        json.dump(file_data, file)
